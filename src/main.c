7102e0e1be51c280c3448a09381a5ccb1db14aa6
weslykim <kimjinse1234@naver.com>
2024-03-22 09:33:12 +0900

    ㅇㅇ

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+/**
+ * @brief printf함수, signal함수, 타이머, static struct를 활용하기 위한 헤더파일을 
+ * 포함시켰습니다.
+ * game,point, 필드의 크기, 처음 시작했을때 꼬리의길이, 꼬리의 변화를 주는 변수,
+ * 게임 시작과 끝을 정의하고 미끼의 위치,snakedirection(sdir), update변수,
+ * disdplay_menu변수, display_snake변수를 가져와서 사욯했습니다. 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -26,7 +34,17 @@ extern int snakeY[];
 int update(int signum);
 int display_menu();
 void display_snake();
-
+/**
+ * @brief game_start 변수를 정의하였습니다. itimerval 구조체는 
+ * POSIX 시스템에서 타이머 값을 설정하고 제어하는 데 사용됩니다. 
+ * 이 구조체는 시간 간격을 나타내는 두 개의 필드(it.value it.interval)을 포함하며
+ * 이 구조체를 정적변수(static struct)화 시켜서 정의했습니다.
+ * 또한 signal함수를 사용하여 가상타이머가 끝나면(SIGVTALRM) update함수를 불러오도록
+ * 설정하였습니다.
+ * 또한 setitmer 함수를 사용하여 타이머를 설정하였고 타이머의 종류(ITIMER_VIRTUAL(가상
+ * 타이머)),timer 구조체의 주소(&timer), 이전 타이머의 설정(보통 NULL로 표현한다.)
+ * 으로 구성했습니다.
+*/
 int game_start()
 {
     // signal 설정
@@ -51,7 +69,9 @@ int game_start()
     }
     game = GAME_END;
 }
-
+/**
+ * 
+*/
 int main(void)
 {
     int menu = 1;
