c0f5eb5dbbe61e76fe7a33f2fb8dc9c42bfb107a
SJH984 <dokdae0624@naver.com>
2024-03-22 10:43:09 +0900

    add ui display_menu

--- src/display_menu.c
+++ src/display_menu.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int display_menu()
-{
+
+int display_menu() {
+
     int menu;
+    
+     system("clear");
 
-    system("clear");
 
-    printf("--------------------\n");
-    printf("Snake mbg\n");
-    printf("-------------------\n");
+    printf("\n\n\n");
+   
+    printf("  SSSS  N   N   AAA   K   k  EEEEE    GGG    AAA   M   M  EEEEE\n");
+    printf(" S      NN  N  A   A  K  K   E       G      A   A  MM MM  E    \n");
+    printf("  SSS   N N N  AAAAA  KK     EEEE    GGGG   AAAAA  M M M  EEEE \n");
+    printf("     S  N  NN  A   A  K  K   E      G    G  A   A  M   M  E    \n");
+    printf(" SSSS   N   N  A   A  K   K  EEEEE   GGGG   A   A  M   M  EEEEE\n");
+
+
+    
+    
+    printf("\tWelcome to Snake Game!\n");
+    
+    printf("------------------------------------------------------\n");
+    printf("\tAre You Ready To Play The Snake Game?\n");
+    printf("------------------------------------------------------\n");
     printf("\t\tGAME MENU\n");
-    printf("-------------------\n");
-    printf("1. Game Start\n");
-    printf("2. Record\n");
-    printf("3. Quit\n");
-    printf("-------------------\n");
+    printf("------------------------------------------------------\n");
+    printf("\t\t1. Game Start\n");
+    printf("\t\t2. Record\n");
+    printf("\t\t3. Quit\n");
+    printf("------------------------------------------------------\n");
 
+    printf("Press any key to start...\n");
     
     scanf("%d", &menu);
     return menu;
 }
+
+
